5031958b19fd8afd678da91c0fc0444a32907cd8
Yomi-09 <alicia345gavidia@gmail.com>
2026-03-02 11:11:25 -0600

    Lab2: Sistema academico con vue.js, boostrap, alertify indexedDB, validaciones en formulario inscripción, matricula.

--- componentes/alumnos.js
+++ componentes/alumnos.js
@@ -50,7 +50,6 @@ const alumnos = {
                 fecha_de_nacimiento: this.alumno.fecha_de_nacimiento,
                 sexo: this.alumno.sexo
             };
-            datos.hash = sha256(JSON.stringify(datos));
             this.buscar = datos.codigo;
             //await this.obtenerAlumnos();
 
