3a952e679c04cad88cf4702c6c3e61486c60e1dc
Yomi-09 <alicia345gavidia@gmail.com>
2026-03-16 11:23:04 -0600

    Base de datos Academica

--- js/db.js
+++
@@ -1,6 +0,0 @@
-const db = new Dexie("USSS094824_yomi", 1);
-
-db.version(1).stores({
-    autores: "++idAutor, codigo, nombre, pais, telefono",
-    libros: "++idLibro, idAutor, isbn, titulo, editorial, edicion"
-});
