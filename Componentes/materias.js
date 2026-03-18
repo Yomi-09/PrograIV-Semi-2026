b12eb0cfdde8e49df28a32730e15ebee4ed230b7
Yomi-09 <alicia345gavidia@gmail.com>
2026-03-18 08:47:49 -0600

    Lab. I, SISTEMA ACADEMICO PHP + MySQL

--- Componentes/materias.js
+++ Componentes/materias.js
@@ -65,7 +65,7 @@ const materias = {
             <div class="col-6">
                 <form id="frmMaterias" @submit.prevent="guardarMateria" @reset.prevent="limpiarFormulario">
                     <div class="card text-bg-dark mb-3" style="max-width: 36rem;">
-                        <div class="card-header">REGISTRO DE ALUMNOS</div>
+                        <div class="card-header">REGISTRO DE MATERIAS</div>
                         <div class="card-body">
                             <div class="row p-1">
                                 <div class="col-3">
